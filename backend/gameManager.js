4ec94f39970f0ca7187d4a3d6b90a8c440b4dad3
Swastik <swastik1283kot@gmail.com>
2025-09-02 00:27:22 +0530

    Included Snake and ladders array ,handleroll function ,newPos to constantly save updated position of players,emitted the position to frontend using socket

--- backend/gameManager.js
+++ backend/gameManager.js
@@ -1,28 +1,62 @@
 import { Player } from "./playerManager.js";
 
+const stairs={
+    3:22,
+    4:35,
+    5:44
+};
+
+
+const snakes={
+    23:12,
+    6:2,
+    10:3,
+}
+
 export class GameManager{
     constructor(io){
         this.io=io;
         this.players={};
         this.currentTurn=null;
     }
-
+   
     addplayer(socket){
         if(!this.players[socket.id]){
            this.players[socket.id]=new Player(socket.id,socket)
+           socket.emit('your-id',socket.id);
+           socket.emit('players-update',Object.keys(this.players).map(id=>({
+            id,
+            score:this.players[id].score,
+            movesleft:this.players[id].movesleft,
+            position:this.players[id].position
+           })));
+
            this.broadcastPlayers();
            if(Object.keys(this.players).length===2){
             this.startgame()
            }
         }
     }
+
+    
     
     removePlayer(id){
         delete this.players[id];
         this.broadcastPlayers();
         
     }
-    
+    notifyturn(){
+        const ids=Object.keys(this.players);
+        ids.forEach(id=>{
+            if(id=== this.currentTurn){
+                this.players[id].socket.emit("your-turn",{turn:id});
+
+            }else{
+                this.players[id].socket.emit("wait-turn",{turn:this.currentTurn});
+            }
+
+        });
+    }
     handleroll(socket){
         if(socket.id!==this.currentTurn){
             socket.emit("not-your-turn");
@@ -36,12 +70,37 @@ export class GameManager{
             socket.emit("no-moves-left");
             return ;
         }
+
+        let newPos=player.position+dice;
+      
+        if(stairs[newPos]){
+            newPos=stairs[newPos];
+            player.position=newPos;
+        }
+        else if(snakes[newPos]){
+            newPos=snakes[newPos];
+            player.position=newPos
+        }
+          if(newPos<=100){
+            player.position=newPos;
+        }
+
         this.io.emit("player-rolled",{
             playerId:player.id,
             dice,
             score:player.score,
-            movesleft:player.movesleft
+            movesleft:player.movesleft,
+            position:player.position,
         });
+        if(player.position===100){
+            this.io.emit('game-over',{
+                winner:player.id,
+                position:Object.fromEntries(
+                    Object.values(this.players).map(p=>[p.id,p.position])
+                )
+            });
+            return;
+        }
         this.switchTurn()
 
         this.checkGameEnd()
@@ -51,14 +110,17 @@ export class GameManager{
         if(ids.length===2){
            this.currentTurn= (this.currentTurn===ids[0]?ids[1]:ids[0]);
             this.io.emit("turn-change",{turn:this.currentTurn});
+            this.notifyturn();
         }
     }
     
     checkGameEnd(){
         const playerArr=Object.values(this.players);
-        if(playerArr.length===2 &&
+        if(
+            playerArr.length===2 &&
             !playerArr[0].hasMovesleft()&&
-            !playerArr[1].hasMovesleft()){
+            !playerArr[1].hasMovesleft())
+            {
                 const[p1,p2]=playerArr;
                 let winner=null;
             if(p1.score>p2.score ) winner=p1.id;
@@ -69,9 +131,14 @@ export class GameManager{
                 scores:{
                     [p1.id]:p1.score,
                     [p2.id]:p2.score
-                }
+                },
+                position:Object.fromEntries([
+                    [p1.id,p1.position],
+                    [p2.id,p2.position],
+                ]),
             })
             }
+            
         
     }
     broadcastPlayers(){
@@ -79,7 +146,8 @@ export class GameManager{
             Object.keys(this.players).map(id=>({
                 id,
                 score:this.players[id].score,
-                movesleft:this.players[id].movesleft
+                movesleft:this.players[id].movesleft,
+               position:this.players[id].position
             })));
 
     }
@@ -89,7 +157,7 @@ export class GameManager{
         if(playerIds.length==2){
             this.currentTurn=playerIds[0];
             this.io.emit("gamestart",{players:playerIds,turn:this.currentTurn});
-
+            this.notifyturn();
         }
     }
  
